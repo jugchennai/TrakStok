f27aad24897df41050dd19988804b330e7af4830
rajmahendra <rajmahendra@gmail.com>
2013-06-03 12:22:08 +0530

    Deleting old JFX project and adding JRebirth

---
+++ TrakStok-JavaFX/src/main/java/in/jugchennai/javamoney/trakstok/SampleFontsLoader.java
@@ -0,0 +1,15 @@
+package in.jugchennai.javamoney.trakstok;
+
+import org.jrebirth.core.resource.font.FontName;
+
+/**
+ * The class <strong>SampleFontsLoader</strong>.
+ * 
+ * @author
+ */
+public enum SampleFontsLoader implements FontName {
+
+    /** The Dink font. */
+    DINk;
+
+}
